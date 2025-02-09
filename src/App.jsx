80569d89904a10a09c2ab3ce2b1eccd4f482f5ea
Sanya Saeed <120942171+Sanya2911@users.noreply.github.com>
2025-02-09 18:35:46 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Technologies from "./components/Technologies";
-import Experience from "./components/experience";
+import Experience from "./components/Experience";
 import Projects from "./components/Projects";
-import Contact from "./components/contact";
+import Contact from "./components/Contact";
 const App = () => {
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased first-letter selection:first-letter:bg-cyan-300 selection:text-cyan-900">
